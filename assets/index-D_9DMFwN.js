5f6ff8329ec045912a2f1d6d0ba1a294e2d56ebb
VladyslavKozlov <vladislavkozlov2101@gmail.com>
2025-06-03 15:32:13 +0300

    Updates

---
+++ assets/index-D_9DMFwN.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BJKbAAWm.js";const s="_root_fpycs_1",e={root:s},n=({className:o=""})=>t.jsx("div",{className:`${e.root} ${o}`,children:"Recomendations"});export{n as default};
--- assets/index-gP0fTY_N.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-IcXjOPUp.js";const s="_root_fpycs_1",e={root:s},n=({className:o=""})=>t.jsx("div",{className:`${e.root} ${o}`,children:"Recomendations"});export{n as default};
